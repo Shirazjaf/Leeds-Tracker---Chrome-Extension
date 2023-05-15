c65c2c22818f263403b2215011f20c6737f542f2
Shirazjaf <ishirazjaf@gmail.com>
2023-05-16 00:41:21 +0530

    Added Delete button

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+const deleteBtn = document.getElementById("del-btn")
 
-console.log(ulEl)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("Double clicked")
+    localStorage.clear()
+    myLeads = ""
+    renderLeads()
+})
+
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEL.value)
     inputEL.value=""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+
+
     renderLeads()
+    console.log( localStorage.getItem("myLeads"))
 } )
 
 function renderLeads() {
