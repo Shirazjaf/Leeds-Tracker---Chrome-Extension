7f6feaf804383e11175358713e4b38e165fac194
Shirazjaf <ishirazjaf@gmail.com>
2023-05-15 17:18:47 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let myLeads = []
+const inputEL = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+
+console.log(ulEl)
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEL.value)
+    inputEL.value=""
+    renderLeads()
+} )
+
+function renderLeads() {
+    let listItems = ""
+for (i=0; i < myLeads.length; i++) {
+    // listItems +=  "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + " " + "</a></li>"
+    listItems +=  `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]} </a>
+        </li>
+    `
+
+}
+
+ulEl.innerHTML = listItems
+
+}
